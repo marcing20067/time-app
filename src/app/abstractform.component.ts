7702fb1dc1aca4b9e9d5be4f946c17cc5b171a65
Marcin8013 <marcing2067@wp.pl>
2021-01-30 22:19:42 +0100

    .

--- src/app/abstractform.component.ts
+++ src/app/abstractform.component.ts
@@ -1,8 +1,35 @@
+import { NgForm } from '@angular/forms';
+import { Task } from './models/task';
+
 export abstract class AbstractFormComponent {
+    model: Partial<Task> = {};
     minTime: string = '';
     maxTime: string = '';
     days_of_month: number[] = [...Array(31 + 1).keys()];
     month_of_years: number[] = [...Array(12 + 1).keys()];
+
+    daysInMonth(e: HTMLInputElement, year: number = new Date().getFullYear()) {
+        setTimeout(() => {
+            const model_num = this.model.month_num as number;
+            const days: number = new Date(year, model_num, 0).getDate();
+            this.days_of_month = [...Array(days + 1).keys()];
+            this.days_of_month.shift();
+            if (days < this.model.day_num) {
+                console.log(this.model.day_num);
+                this.model.day_num = undefined;
+                console.log(this.model.day_num);
+            }
+        }, 1)
+    }
+
+    getSubmitBoolean(form: NgForm) {
+        let x: Boolean = true
+
+        if (form.invalid === false && this.minTime !== this.maxTime) {
+            this.model.day_num === undefined ? x = true : x = false
+        }
+        return x
+    }
 
     constructor() {
         this.days_of_month.shift();
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { AbstractFormComponent } from '../abstractform.component';
-import { Task } from './../models/task'
-
+import { Task } from '../models/task';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -11,23 +9,6 @@ import { Task } from './../models/task'
 export class FormComponent extends AbstractFormComponent {
   @Output()
   newTask = new EventEmitter<Task>();
-  model: Partial<Task> = {};
-
-
-
-  daysInMonth(e: HTMLInputElement, year: number = new Date().getFullYear()) {
-    setTimeout(() => {
-      const model_num = this.model.month_num as number;
-      const days: number = new Date(year, model_num, 0).getDate();
-      this.days_of_month = [...Array(days + 1).keys()];
-      this.days_of_month.shift();
-      if (days < this.model.day_num) {
-        console.log(this.model.day_num);
-        this.model.day_num = undefined;
-        console.log(this.model.day_num);
-      }
-    }, 1)
-  }
 
   send(): void {
     this.model.done = false;
@@ -46,13 +27,5 @@ export class FormComponent extends AbstractFormComponent {
     this.maxTime = ''
   }
 
-  getSubmitBoolean(taskForm: NgForm) {
-    let x: Boolean = true
-
-    if (taskForm.invalid === false) {
-      this.model.day_num === undefined ? x = true : x = false
-    }
-    return x
-  }
 }
 
